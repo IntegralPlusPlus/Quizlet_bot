9e68a068b0b9394255a1cba1aa04c39716740b4a
IntegralPlusPlus <integralplusc@gmail.com>
2025-04-13 01:37:43 +0300

    Added state-machine with module-creating and adding words to new module

--- app/handlers.py
+++ app/handlers.py
@@ -10,18 +10,17 @@ import app.database.requests as requests
 
 class CreateModule(StatesGroup):
     name = State()
-class CreateWord(StatesGroup):
     word = State()
     translation = State()
 
 router = Router()
 
 @router.message(CommandStart())
-async def start(message: Message):
+async def start_menu(message: Message):
     await requests.set_user(message.from_user.id)
-    await message.answer("Здравствуйте! Приглашаем вас использовать Telegram-бота, выполняющего функции личного Quizlet'а! \n" +
-                         'Предлагаю начать',
-                         reply_markup = kb.start_menu)
+    
+    await message.answer("Здравствуйте! Приглашаем вас использовать Telegram-бота, выполняющего функции личного Quizlet'а! \n" + \
+                        'Предлагаю начать', reply_markup = kb.start_menu)
     await message.delete()
 
 @router.message(F.text == 'Создать новый модуль')
@@ -32,28 +31,42 @@ async def create_module(message: Message, state: FSMContext):
 @router.message(CreateModule.name)
 async def module_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
-    module_name = (await state.get_data())['name']
-    await requests.set_module(message.from_user.id, module_name)
+    module_name = message.text #(await state.get_data())['name']
+    #await requests.set_module(message.from_user.id, module_name)
 
-    await message.answer(f"Модуль {module_name} успешно создан!\n", reply_markup=kb.start_menu)
+    await message.answer(f"Модуль {module_name} успешно создан!\n", reply_markup=kb.to_start_menu)
+    #await state.set_state(CreateWord.word)
 
-"""    
-@router.message(CreateWord.word)
-async def module_word(message: Message, state: FSMContext):
-    await state.update_data(word = message.text)
-    await message.answer("Введите перевод")
-    await state.set_state(CreateWord.translation)
+@router.callback_query(F.data == 'to_start_menu')
+async def main_menu(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await callback.message.answer("Выберите один из пунктов меню:", reply_markup = kb.start_menu)
 
-@router.message(CreateWord.translation)
-async def module_translation(message: Message, state: FSMContext):
-    await state.update_data(translation = message.text)
+@router.callback_query(F.data == 'add_word')
+async def add_word(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await callback.message.answer("Введите слово, которое вы хотите добавить в модуль", reply_markup=ReplyKeyboardRemove())
+    await state.set_state(CreateModule.word)
+
+@router.message(CreateModule.word)
+async def add_translation(message: Message, state: FSMContext):
+    await state.update_data(word=message.text)
+    await message.answer("Введите перевод слова")
+    await state.set_state(CreateModule.translation)
+
+@router.message(CreateModule.translation)
+async def write_word_and_translation(message: Message, state: FSMContext):
+    await state.update_data(translation=message.text)
     data = await state.get_data()
-    name = data.get('name')
+    module_name = data.get('name')
     word = data.get('word')
     translation = data.get('translation')
     
-    await message.answer(f"Слово: {word}\n" +
-                         f"Перевод: {translation}\n" +
-                         f"Успешно добавлены в модуль: {name}\n" +
-                         "Теперь вы можете продолжить работу с ботом по одному из пунктов меню")
-"""
+    await message.answer(
+        f"Слово '{word}' с переводом '{translation}' успешно добавлено в модуль '{module_name}'\n"
+        "Вы можете продолжить работу с ботом, выбрав один из пунктов меню",
+        reply_markup=kb.to_start_menu
+    )
+
+    #await state.clear()
--- app/keyboards.py
+++ app/keyboards.py
@@ -5,7 +5,15 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 start_menu = ReplyKeyboardMarkup(
     keyboard = [
         [KeyboardButton(text = 'Создать новый модуль')],
+        [KeyboardButton(text = 'Добавить слово в существующий модуль')],
         [KeyboardButton(text = 'Повторить уже существующий модуль')],
     ],
     resize_keyboard = True, input_field_placeholder = "Выберите нужный пункт меню..."
+)
+
+to_start_menu = InlineKeyboardMarkup(
+    inline_keyboard = [
+        [InlineKeyboardButton(text = 'Вернуться в главное меню', callback_data = 'to_start_menu'),
+         InlineKeyboardButton(text = 'Добавить новое слово в модуль', callback_data = 'add_word')],
+    ],
 )
