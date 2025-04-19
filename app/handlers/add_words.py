9dcd35e3624bd6ab0d5efd4971931cdc44dce4e9
IntegralPlusPlus <integralplusc@gmail.com>
2025-04-19 14:52:44 +0300

    Added autodelete message to all handlers

--- app/handlers/add_words.py
+++ app/handlers/add_words.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 import app.database.requests as requests
+import app.basic_functions as basicFuns
 
 from app.handlers.main_handlers import router
 
@@ -16,16 +17,24 @@ class CreateModule(StatesGroup):
 
 @router.message(F.text == 'Создать новый модуль')
 async def create_module(message: Message, state: FSMContext):
-    await message.answer("Введите название модуля", reply_markup=ReplyKeyboardRemove())
+    await message.delete()
+    await basicFuns.change_message(state,
+                                   message,
+                                   basicFuns.MessageType.MESSAGE,
+                                   "Введите название модуля",
+                                   ReplyKeyboardRemove())
+
+
     await state.set_state(CreateModule.name)
 
 @router.message(F.text == 'Добавить слово в существующий модуль')
-async def show_modules_to_add_words(message: Message):
-    await message.answer('⌛️ Подготавливаю список модулей...', reply_markup=ReplyKeyboardRemove())
-
-    await message.answer("Выберите модуль, в который хотите добавить слово", 
-                         reply_markup=await kb.show_modules(message.from_user.id, kb.ShowModulesStates.TO_ADD_WORDS))
+async def show_modules_to_add_words(message: Message, state: FSMContext):
     await message.delete()
+    await basicFuns.change_message(state,
+                                   message,
+                                   basicFuns.MessageType.MESSAGE,
+                                   "Выберите модуль, в который хотите добавить слово",
+                                   await kb.show_modules(message.from_user.id, kb.ShowModulesStates.TO_ADD_WORDS))
 
 @router.callback_query(F.data.startswith('addwrdmdl_'))
 async def add_word_to_current_module(callback: CallbackQuery, state: FSMContext):
@@ -34,33 +43,50 @@ async def add_word_to_current_module(callback: CallbackQuery, state: FSMContext)
     module_name = await requests.get_module_name_by_id(callback.from_user.id, module_id)
 
     await state.update_data(name=module_name)
-    await callback.message.answer(f"Вы выбрали модуль '{module_name}'.\nВведите слово, которое вы хотите добавить в модуль", 
-                                  reply_markup=ReplyKeyboardRemove())
+    await basicFuns.change_message(state,
+                                   callback,
+                                   basicFuns.MessageType.CALLBACK,
+                                   f"Вы выбрали модуль '{module_name}'.\nВведите слово, которое вы хотите добавить в модуль",
+                                   ReplyKeyboardRemove())
     await state.set_state(CreateModule.word)
 
 @router.message(CreateModule.name)
 async def module_name(message: Message, state: FSMContext):
+    await message.delete()
     await state.update_data(name=message.text)
     module_name = message.text
     
     await requests.set_module(message.from_user.id, message.from_user.username, module_name)
 
-    await message.answer(f"✅ Модуль '{module_name}' успешно создан!\n", reply_markup=kb.add_new_word_to_module)
+    await basicFuns.change_message(state,
+                                   message,
+                                   basicFuns.MessageType.MESSAGE,
+                                   f"✅ Модуль '{module_name}' успешно создан!",
+                                   kb.add_new_word_to_module)
 
 @router.callback_query(F.data == 'add_word')
 async def add_word(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
-    await callback.message.answer("Введите слово, которое вы хотите добавить в модуль", reply_markup=ReplyKeyboardRemove())
+    await basicFuns.change_message(state,
+                                   callback,
+                                   basicFuns.MessageType.CALLBACK,
+                                   "Введите слово, которое вы хотите добавить в модуль",
+                                   ReplyKeyboardRemove())
     await state.set_state(CreateModule.word)
 
 @router.message(CreateModule.word)
 async def add_translation(message: Message, state: FSMContext):
+    await message.delete()
     await state.update_data(word=message.text)
-    await message.answer("Введите перевод слова")
+    await basicFuns.change_message(state,
+                                   message,
+                                   basicFuns.MessageType.MESSAGE,
+                                   "Введите перевод слова")
     await state.set_state(CreateModule.translation)
 
 @router.message(CreateModule.translation)
 async def write_word_and_translation(message: Message, state: FSMContext):
+    await message.delete()
     await state.update_data(translation=message.text)
     data = await state.get_data()
     module_name = data.get('name')
@@ -69,7 +95,9 @@ async def write_word_and_translation(message: Message, state: FSMContext):
 
     await requests.set_word(message.from_user.id, module_name, word, translation)
 
-    await message.answer(
-        f"✅ Слово '{word}' с переводом '{translation}' успешно добавлено в модуль '{module_name}'\n"
-        "Вы можете продолжить работу с ботом, выбрав один из пунктов меню",
-        reply_markup=kb.add_new_word_to_module)
+    await basicFuns.change_message(state,
+                                   message,
+                                   basicFuns.MessageType.MESSAGE,
+                                   f"✅ Слово '{word}' с переводом '{translation}' успешно добавлено в модуль '{module_name}'\n"
+                                   "Вы можете продолжить работу с ботом, выбрав один из пунктов меню",
+                                   kb.add_new_word_to_module)
--- app/handlers/delete_words.py
+++ app/handlers/delete_words.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 import app.database.requests as requests
+import app.basic_functions as basicFuns
 
 from app.handlers.main_handlers import router
 
@@ -16,28 +17,27 @@ class DeleteWords(StatesGroup):
 @router.message(F.text == 'Удалить модуль или слово в модуле')
 async def show_modules_to_delete(message: Message, state: FSMContext):
     await message.delete()
-    await message.answer('⌛️Подготавливаю список модулей...', reply_markup=ReplyKeyboardRemove())
-
-    await message.answer("Выберите модуль, который вы хотите удалить или модуль, в котором хотите удалить слово", 
-                          reply_markup=await kb.show_modules(message.from_user.id, kb.ShowModulesStates.TO_DELETE))
-    await state.set_state(DeleteWords.module_id)
+    await basicFuns.change_message(state,
+                                   message,
+                                   basicFuns.MessageType.MESSAGE,
+                                   "Выберите модуль, который вы хотите удалить или модуль, в котором хотите удалить слово",
+                                   await kb.show_modules(message.from_user.id, kb.ShowModulesStates.TO_DELETE))
 
 @router.callback_query(F.data.startswith('dltmdl_'))
 async def delete_module(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
     
     _module_id = callback.data.removeprefix('dltmdl_').strip()
-    
-    if not _module_id:
-        await callback.message.answer("⚠️ Ошибка: имя модуля не распознано!")
-        return
 
     _module_name = await requests.get_module_name_by_id(callback.from_user.id, int(_module_id))
     await state.update_data(module_id=_module_id)
     await state.update_data(module_name=_module_name)
 
-    await callback.message.answer(f"Вы хотите удалить весь модуль '{_module_name}' или конкретное слово в нем?\n",
-                                  reply_markup=kb.delete_module_or_word)
+    await basicFuns.change_message(state,
+                                   callback,
+                                   basicFuns.MessageType.CALLBACK,
+                                   f"Вы хотите удалить весь модуль '{_module_name}' или конкретное слово в нем?\n",
+                                   kb.delete_module_or_word)
 
 @router.callback_query(F.data == 'delete_all_module')
 async def delete_all_module(callback: CallbackQuery, state: FSMContext):
@@ -45,19 +45,14 @@ async def delete_all_module(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     module_id = data.get('module_id')
     module_name = data.get('module_name')
-
-    if not module_id:
-        await callback.message.answer("⚠️ Ошибка: модуль не выбран.")
-        await state.clear()
-        return
-
-    await callback.message.answer(f"⌛️ Удаление модуля '{module_name}', это может занять некоторое время...")
  
     await requests.delete_module(callback.from_user.id, int(module_id))
     
-    await callback.message.answer(f"✅ Модуль '{module_name}' успешно удален!\n",
-                                  reply_markup=kb.to_start_menu)
-    await state.clear()
+    await basicFuns.change_message(state,
+                                   callback,
+                                   basicFuns.MessageType.CALLBACK,
+                                   f"✅ Модуль '{module_name}' успешно удален!\n",
+                                   kb.to_start_menu)
 
 @router.callback_query(F.data == 'delete_current_word')
 async def delete_current_word(callback: CallbackQuery, state: FSMContext):
@@ -66,13 +61,11 @@ async def delete_current_word(callback: CallbackQuery, state: FSMContext):
     module_id = data.get('module_id')
     module_name = data.get('module_name')
 
-    if not module_id:
-        await callback.message.answer("⚠️ Ошибка: модуль не выбран.")
-        await state.clear()
-        return
-
-    await callback.message.answer(f"Выберите слово-перевод, которые вы хотите удалить из модуля '{module_name}'",
-                                  reply_markup = await kb.show_words(int(module_id)))
+    await basicFuns.change_message(state,
+                                   callback,
+                                   basicFuns.MessageType.CALLBACK,
+                                   f"Выберите слово-перевод, которые вы хотите удалить из модуля '{module_name}'",
+                                   await kb.show_words(int(module_id)))
 
 @router.callback_query(F.data.startswith('dltwrd_'))
 async def delete_word(callback: CallbackQuery, state: FSMContext):
@@ -83,10 +76,11 @@ async def delete_word(callback: CallbackQuery, state: FSMContext):
     
     word_id = int(callback.data.removeprefix('dltwrd_').strip())
     word_name = await requests.get_word_by_id(int(module_id), int(word_id))
-
     await requests.delete_word(int(module_id), int(word_id))
     
-    await callback.message.answer(f"✅ Слово '{word_name.word}' и перевод '{word_name.translation}' " + \
-                                  f"успешно удалены из модуля '{module_name}'!\n",
-                                  reply_markup=kb.to_start_menu)
-    await state.clear()
+    await basicFuns.change_message(state,
+                                   callback,
+                                   basicFuns.MessageType.CALLBACK,
+                                   f"✅ Слово '{word_name.word}' и перевод '{word_name.translation}' " + \
+                                   f"успешно удалены из модуля '{module_name}'!\n",
+                                   kb.to_start_menu)
